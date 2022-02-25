ca6e68fd58a15698ddcf0cb7ce100c54404bd323
JL-Sandin <46983354+JL-Sandin@users.noreply.github.com>
2022-02-24 21:23:02 -0600

    Update Polynomial.pl

--- Polynomial.pl
+++ Polynomial.pl
@@ -1,15 +1,26 @@
 degree([K|Poly], N) :- 
-  K \= 0, length(Poly, N).
+  	K \= 0, length(Poly, N).
 degree([_|Poly], N) :- 
-  degree(Poly, N).
+  	degree(Poly, N).
 
 plus([KA|PA], [KB|PB], [KS|PS]) :- 
-  length(PA, NA), length(PB, NB), NA > NB,
+  	length(PA, NA), length(PB, NB), NA > NB,
 	plus(PA, [KB|PB], PS), KS is KA.
 
 plus([KA|PA], [KB|PB], [KS|PS]) :- 
-  length(PA, NA), length(PB, NB), NA < NB,
+  	length(PA, NA), length(PB, NB), NA < NB,
 	plus([KA|PA], PB, PS), KS is KB.
 
 plus([KA|PA], [KB|PB], [KS|PS]) :- 
-  plus(PA, PB, PS), KS is KA + KB.
+  	plus(PA, PB, PS), KS is KA + KB.
+  
+minus([KA|PA], [KB|PB], [KS|PS]) :- 
+	length(PA, NA), length(PB, NB), NA > NB,
+	minus(PA, [KB|PB], PS), KS is KA.
+	
+minus([KA|PA], [KB|PB], [KS|PS]) :- 
+	length(PA, NA), length(PB, NB), NA < NB,
+	minus([KA|PA], PB, PS), KS is -KB.
+	
+minus([KA|PA], [KB|PB], [KS|PS]) :- 
+	minus(PA, PB, PS), KS is KA - KB.
