f8112c45fba6a752c645305a4c477013ab9dba11
Sandin <jlsandin@tutanota.com>
2022-03-27 00:34:12 -0600

    domino.pl

--- Domino.pl
+++ Domino.pl
@@ -1,5 +1,72 @@
-%%% Prolog progam that plays Domino by entering the 
+%%% Prolog progam that plays Domino by entering the
 %%% tiles the players have.
+
+%%% 0
+domino([0,0]).
+
+%%% 1
+domino([0,1]).
+domino([1,1]).
+
+%%% 2
+domino([0,2]).
+domino([1,2]).
+domino([2,2]).
+
+%%% 3
+domino([0,3]).
+domino([1,3]).
+domino([2,3]).
+domino([3,3]).
+
+domino([0,4]).
+domino([1,4]).
+domino([2,4]).
+domino([3,4]).
+domino([4,4]).
+
+domino([0,5]).
+domino([1,5]).
+domino([2,5]).
+domino([3,5]).
+domino([4,5]).
+domino([5,5]).
+
+domino([0,6]).
+domino([1,6]).
+domino([2,6]).
+domino([3,6]).
+domino([4,6]).
+domino([5,6]).
+domino([6,6]).
+
+
+
+
+read_tiles:-
+    L=[],
+    read(X),
+    is_list(X),
+    is_domino_l(X),
+    append(X,L,F),
+    write('Current '),write(F),nl,
+    assertz(F).
+
+is_domino_l([]):-!.
+is_domino_l([D|R]):-
+    is_list(D),
+    length(D,2),
+    is_domino(D),
+    is_domino_l(R),
+    !.
+
+is_domino([X|Y]):-
+    X < 7,
+    Y < 7,
+    X >= 0,
+    Y >= 0.
+
+
 
 /* The alpha-beta algorithm from textbook
 */
@@ -7,7 +74,7 @@ alphabeta( Pos, Alpha, Beta, GoodPos-GoodPosMeta, Val):-
 	moves( Pos, PosList), !,
 	boundedbest( PosList, Alpha, Beta, GoodPos-GoodPosMeta, Val)
 	;
-	h( Pos, Val).                              % Static value of Pos 
+	h( Pos, Val).                              % Static value of Pos
 
 boundedbest( [Pos-Meta | PosList], Alpha, Beta, GoodPos-GoodPosMeta, GoodVal):-
 	alphabeta( Pos, Alpha, Beta, _, Val),
@@ -21,19 +88,19 @@ goodenough( _, Alpha, Beta, Pos, Val, Pos, Val)  :-
 	max_to_move( Pos), Val < Alpha, !.                 % Minimizer attained lower bound
 
 goodenough( PosList, Alpha, Beta, Pos, Val, GoodPos, GoodVal)  :-
-	newbounds( Alpha, Beta, Pos, Val, NewAlpha, NewBeta),    % Refine bounds  
+	newbounds( Alpha, Beta, Pos, Val, NewAlpha, NewBeta),    % Refine bounds
 	boundedbest( PosList, NewAlpha, NewBeta, Pos1, Val1),
 	betterof( Pos, Val, Pos1, Val1, GoodPos, GoodVal).
 
 newbounds( Alpha, Beta, Pos, Val, Val, Beta)  :-
-	min_to_move( Pos), Val > Alpha, !.                 % Maximizer increased lower bound 
+	min_to_move( Pos), Val > Alpha, !.                 % Maximizer increased lower bound
 
 newbounds( Alpha, Beta, Pos, Val, Alpha, Val)  :-
-	max_to_move( Pos), Val < Beta, !.                 % Minimizer decreased upper bound 
+	max_to_move( Pos), Val < Beta, !.                 % Minimizer decreased upper bound
 
-newbounds( Alpha, Beta, _, _, Alpha, Beta).          % Otherwise bounds unchanged 
+newbounds( Alpha, Beta, _, _, Alpha, Beta).          % Otherwise bounds unchanged
 
-betterof( Pos, Val, _, Val1, Pos, Val)  :-        % Pos better than Pos1 
+betterof( Pos, Val, _, Val1, Pos, Val)  :-        % Pos better than Pos1
 	min_to_move( Pos), Val > Val1, !
 	;
 	max_to_move( Pos), Val < Val1, !.
