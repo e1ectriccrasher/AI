7a2ea1072fd91a1e30a3363188e6fbf4b066122d
Victoria Ordorica Pardo <35547664+victoriaordoricapardo@users.noreply.github.com>
2022-04-05 15:27:36 -0500

    Add files via upload

--- Domino.pl
+++ Domino.pl
@@ -1,23 +1,15 @@
-/*-------------------- Instructions --------------------*/
+/*-------------------Instruction--------------------*/
 % You can start the game using:
 % startGame.
-%
-%
-%
-%
-% Falta agregar el comer ficha 
-%Hay  varios SINGLETON VARIABLES que no se como quitar
-%Lo ultimo poner en ingles
-%printGameDetails falta
-%
-/*-------------------- Board & Game Function --------------------*/
+
+/*-------------------- Board & Game Engine --------------------*/
 
 % Main predicate:
 startGame():-
     % Create the board State: [Layout, Possible Pieces, userHand]
     initState(State),
     % Add 7 new pieces to the hand
-    draw(State,0,7, NewState),
+    draw(State,0,3, NewState), %Debo cambiar a 7 piezas
     % Check whos first and start game.
     chooseTurn(NewState).
 
@@ -26,7 +18,7 @@ initState([[-1],[[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [1, 1],
 
 % Print the Game State
 printGameDetails([Layout, Possible, Hand]):-
-    format('Layout: ~w~nPossible Pieces: ~w~nMy Hand: ~w~n', [Layout, Possible, Hand]).
+    format('Layout: ~w~nPossible Pieces: ~w~nMy Hand: ~w~n',[Layout, Possible, Hand]).
 
 % draw(0, N) := Take N pieces querying the user.
 draw(State, Current, Current, State).
@@ -57,7 +49,7 @@ chooseTurn(State):-
 myTurn(State):-
     printGameDetails(State), nl,
     write("-------------My Turn--------------"), nl,
-    selectMyMove(State).
+    selectMyMove(State). %We add the move to the board/Layout
 
 enemyTurn(State):-
     printGameDetails(State) ,nl,
@@ -67,21 +59,11 @@ enemyTurn(State):-
 /*-------------------- My moves --------------------*/
 
 % Check if we can play and make an optimal move.
-selectMyMove( State ):-
-    %[_, _, Hand] = State,
-    % if hand is empty, got to performMyCommand(bla, draw, bla),
+selectMyMove(State):-
 
-    % Check if we can play something, if not, performMyCommand(bla, pass, bla),
+	Move = [6,6], %falta agregar el paso de inicio agregar toda la funcionalidad.
+    	performMyCommand(State,Move).
 
-    % Get a smart sublist of the hand, checking what can be played,
-
-    % Run alfabeta with that info, getting the best tile we can play
-
-    %genStates(Hand, InterestingMoves),
-    % alphabeta(4, -3000, 3000, InterestingMoves, Possible , BestMove, 1);
-    % Last but not least, perform that command
-    performMyCommand(State, Move).
-    %printGameDetails(State).
 
 % performMyCommand(State, Side, Move)
 performMyCommand([Layout, Possible, Hand], [Side1, Side2]):-
@@ -116,7 +98,7 @@ performEnemyCommand(State, 0) :-
 
 % Insert Domino on the board
 performEnemyCommand( [Layout, Possible, Hand], [Side1, Side2]) :-
-    write("Got to performEnemyCommand with tile"),nl,
+	write("Got to performEnemyCommand with tile- invalid input"),nl,
     delete(Possible, [Side1, Side2], NewPossible), %It deletes the piece played from possible pieces because it is now revealed
     delete(NewPossible, [Side2, Side1], NewPossible2),
     insertDomino([Side1, Side2], Layout, NewLayout), %As it is an insertion on the left it pushes the piece into the layout, creating a new layout.
@@ -169,7 +151,7 @@ deleteListFromList(List1, [ L2H| []], Result):-
 delete(List1, L2H, Result), !.
 deleteListFromList(List1, [L2H |L2T], Result):-
 	deleteListFromList(List1, L2T, SubResult),
-	delete(SubResult, L2H, Result).
+	delete(SubResult, L2H, Result). 
 
 /*-------------------- Alphabeta --------------------*/
 test:-
@@ -279,28 +261,24 @@ alphabeta( Depth, Alpha, Beta,[ [StatesH_Piece, StatesH_Hand] | StatesT], Storag
 	min(Beta,  BestMove1, MinimizedBeta),
 
 	% Compute same level branches:
-	alphabeta( Depth, Alpha, MinimizedBeta, StatesT, Storage, BestMove2, 0),
+alphabeta( Depth, Alpha, MinimizedBeta, StatesT, Storage, BestMove2, 0),
 	min(MinimizedBeta, BestMove2, BestMove).
-
-% Mock method for the heuristic.
-heuristic(Position):-
-Position = 1.
 
 
 % Create possible states per row:
 % genStates( i, o), where i= Hand of the player.
-genStates( Hand , PossibleHands):-
+genStates(Hand , PossibleHands):-
 	genPossibleHands( [], Hand, PossibleHands).
 genPossibleHands(_, [], []).
 genPossibleHands(HandBuffer , [HandH| HandT], PossibleHands):-
 	genPossibleHands( [ HandH | HandBuffer], HandT, PossibleHandsT ),
-	combina(HandBuffer, HandT, AllMinusOne),
+	mix(HandBuffer, HandT, AllMinusOne),
 	PossibleHands = [ [HandH , AllMinusOne] | PossibleHandsT].
 
 % Utility method, used by genStates.
-combina([],Lista,Lista):-!.
-combina([X|Lista1],Lista2,[X|Lista3]) :-
-	combina(Lista1,Lista2,Lista3).
+mix([],List,List):-!.
+mix([X|List1],List2,[X|List3]) :-
+	mix(List1,List2,List3).
 
 
 % Used in alphabeta:
@@ -321,174 +299,126 @@ min( X, Y, Z):-
 	!.
 min(_, Y, Y).
 
-/%
-%Counts the number of elements in a list
-%count([], 0).
-%count( [ Head | Tails ], N):-
-%	count(Tails, N1),
-%	N is N1 + 1.
-%/
-
-% ============================================= Print Matrix(i)
-/*
-This clause traverses the matrix as in the "sum" clause, while printing
-the matrix entries.
-*/
-
-printList([ [] | [] ]):- nl,nl,!.
-
-%End of Row?
-printList([ []|ColumsA]):-
-	nl, printList(ColumsA).
-
-%Main Method
-printList([[EntryA | ColumnsA] | RowsA]):-
-	write(EntryA),write(" "),
-	printList([ColumnsA|RowsA]).
-
-% Emulate a loop.
-repeat.
-
-repeat:-
-	repeat.
-
-
-% Confirm that the Data passed by the user is valid,
-% Give the user a second chance to insert data.
-confirm(Data):-
-write("Is "),
-write(Data),
-write(" correct? (y/n)"),
-nl,
-read(YesNo),
-YesNo == y.
-
-
 /*-------------------- Heuristica --------------------*/
-listaSimpleAListaListas([X,Y|Cola],[A|Cola2]):-
-    creaLista(X,Y,A),
-    listaSimpleAListaListas(Cola,Cola2).
 
-listaSimpleAListaListas([],_):-!.
-
-creaLista(X,Y,[A,B]):-
-    A is X,
-    B is Y.
-
-
-%En esta función se sumarán todos los puntajes de distintos
-%métodos heurísticos. Por ahora van 3
+%Sum of the score of three different heuristic methods
 %i,i,o
-heuristica(Mano,ManoOponente,Res):-
-    repetidas(Mano,ResRep),
-    relacionOponente(ManoOponente,Mano,ResOp),
-    relacionMulas(Mano,ResMul),
-    Res is ResRep+ResOp+ResMul.
+heuristic(Hand,HandOp,Res):-
+    repeated(Hand,ResRep),
+    relationDT(Hand,ResDT),
+    relationOp(HandOp,Hand,ResOp),
+    Res is ResRep+ResOp+ResDT.
 
 
-% Si nos vamos quedando con mulas puede perjudicarnos, así que le
-% asignamos un puntaje negativo al tener dos mulas o más y uno positivo
-% al tener 1 o ninguna.
-%i,o
-relacionMulas(Mano,Res):-
-    mulas(Mano,Count),
+% if we stay with double tales if we lose we would have a higher score,
+% so we assign -1 score if we have 2 or more double tales 
+%1 score if we have 1 or none double tale
+% %i,o
+relationDT(Hand,Res):-
+    doubleTales(Hand,Count),
     Count>=2,
-    Res is -2;
+    Res is -1;
     Res is 1.
-%Cuenta el número de mulas que se tienen llamando al predicado esMula
-%i,o
-mulas([X|Cola], Count):-
-    esMula(X,Res),
-    mulas(Cola,C1),
+
+%Counts number of double tales on the hand
+%%i,o
+doubleTales([X|Tail], Count):-
+    isDT(X,Res),
+    doubleTales(Tail,C1),
     Count is C1+Res.
-mulas([],0):-!.
-%Revisa si el primero y segundo numero de la ficha es el mismo,
-%entonces es mula
-%i,o
-esMula([X|Cola],Res):-
-    segundoNum(Cola,Num),
+doubleTales([],0):-!.
+
+%Checks if the two number on the piece are the same
+%%i,o
+isDT([X|Tail],Res):-
+    second(Tail,Num),
     X=:=Num,
     Res is 1;
     Res is 0.
-segundoNum([X|_],Num):-
+second([X|_],Num):-
     Num is X.
 
-% Si el oponente tiene más fichas entonces Res será positivo para
-% nosotros
+% If the opponent has more pieces we have a positive score
 %i,i,o
-relacionOponente(ManoOponente,ManoPropia,Res):-
-    size(ManoOponente,CountO),
-    size(ManoPropia,CountP),
+relationOp(HandOp,Hand,Res):-
+    size(HandOp,CountO),
+    size(Hand,CountP),
     Res is CountO-CountP.
 
-%Se ve la relación entre el numero de fichas en la mano del usuario contra la
-% cantidad mayor de repetidas, por ejemplo, si tenemos 4 fichas con el numero 5
-% , 2 fichas con el número 2, etc. usariamos 4. Si la relación es de más del 50%
-% para las fichas repetidas con respecto del número de fichas en la mano entonces
-% asignamos un valor positivo al estado.
-%i,i,o
-relacion(Count,Mayor,Rel):-
-    Count/Mayor>2,
-    Rel is -3;
-    Count/Mayor=<2,
-    Rel is 3.
-%Se revisa el numero de fichas con el mismo número en la mano del usuario,
-%Si tenemos mula solo contamos como 1 repeticion
+%Relation between the number of tiles at our hand against the greater number of repeats number.
+%%i,i,o
+relation(Count,Bigger,Rel):-
+    Count/Bigger>2,
+    Rel is -1;
+    Count/Bigger=<2,
+    Rel is 1.
+%Checks number of repeated number
 %i,o
-repetidas(Mano,Rel):-
+repeated(Hand,Rel):-
     Num is 0,
-    cuenta(Mano,Num,Arr),
-    numRepetidas(Arr,Mayor),
-    size(Mano,Count),
-    relacion(Count,Mayor,Rel).
-%Obtiene el tamaño de la lista
-%i,o
-size([_|Cola],Count):-
-    size(Cola,C1),
+    count2(Hand,Num,Arr),
+    numRepeated(Arr,Mayor),
+    size(Hand,Count),
+    relation(Count,Mayor,Rel).
+%size of the list
+%%i,o
+size([_|Tail],Count):-
+    size(Tail,C1),
     Count is C1+1.
 size([],Count):-
     Count is 0,!.
-%Obtiene el número mayor de repeticiones en las fichas, por ejemplo, si tenemos
-% 4 fichas con el numero 5, 2 fichas con el número 2, etc. regresaría 4. No nos
-% interesa tanto qué ficha es la que se repite más, solamente que el número de
-% repeticiones sea alto
+%Gets the largest number of repetitions in the tiles.
 %i,o
-numRepetidas([X|Cola],Mayor):-
-    numRepetidas2(Mayor,X,Cola).
-numRepetidas2(A,B,[X|Cola]):-
+numRepeated([X|Tail],Bigger):-
+    numRepeated2(Bigger,X,Tail).
+numRepeated2(A,B,[X|Tail]):-
     X>=B,!,
-    numRepetidas2(A,X,Cola);
+    numRepeated2(A,X,Tail);
     X=<B,
-    numRepetidas2(A,B,Cola).
-numRepetidas2(Mayor,Mayor,[]):-!.
+    numRepeated2(A,B,Tail).
+numRepeated2(Bigger,Bigger,[]):-!.
 
 
 
-%cuenta las repeticiones que hay por cada numero, las mulas cuentan 1 repeticion
+%Count the repetitions that there are for each nuber, the double tales count as one
 % i,i,o
-cuenta(Mano,Num, Freq):-
+count2(Hand,Num, Freq):-
     Num=:=7, Freq = [];
-    suma(Mano,Num, X),
+    sum(Hand,Num, X),
     Num1 is Num+1,
-    cuenta(Mano, Num1, Cola),
-    Freq = [ X | Cola ].
+    count2(Hand, Num1, Tail),
+    Freq = [ X | Tail ].
 
-%cuenta las repeticiones que hay para un numero en específico, las mulas cuentan 1 repeticion
+%Count the repetitions for a specific number
 % i,i,o
-suma([X|Cola],Num,Count):-
-    suma(Cola,Num,C1),
-    verifica(X,Num),
+sum([X|Tail],Num,Count):-
+    sum(Tail,Num,C1),
+    verify(X,Num),
     Count is C1+1;
-    suma(Cola,Num,C1),
+    sum(Tail,Num,C1),
     Count is C1.
-suma([],_,Count):-
+sum([],_,Count):-
     Count is 0.
-%verifica que un número en específico sea igual a la primera entrada de una ficha
+%verify that a number is equal to the first number on the tile
 %i,o
-verifica([X|Cola],Num):-
+verify([X|Tail],Num):-
     X=:=Num;
-    verifica2(Cola,Num).
-%verifica que un número en específico sea igual a la segunda entrada de una ficha
-%i,o
-verifica2([X|_],Num):-
+    verify2(Tail,Num).
+%verify that a number is equal to the second number on the tile
+verify2([X|_],Num):-
     X=:=Num.
+
+sum_tile([X|Y], V):-
+    V is X + Y.
+
+sum_list1([], []):- !.
+
+sum_list1([X|R], V):-
+    sum_tile(X, SR),
+    sum_list1(R, V1),
+    V = [SR|V1].
+
+
+zs_max_at(Zs,Max,Pos):-
+    maplist(#>=(Max),Zs),
+    nth0(Pos,Zs,Max).
